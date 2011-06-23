87989cb5425b238a6493cb6f8297c48f2e2edc59
newLoki <newloki91@googlemail.com>
2011-06-23 15:04:53 +0200

    Add abstract for parsers and interface

---
+++ Library/Parser/Interface.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * 
+ * @author newloki
+ */
+ 
+Interface Parser_Interface
+{
+    /**
+     * Method to parse given text
+     * @abstract
+     * @return void
+     */
+    public function parse();
+
+}
