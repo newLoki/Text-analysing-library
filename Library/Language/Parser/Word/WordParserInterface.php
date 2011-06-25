67df0a1866421994f1e2302adfd8e6b061842b70
newLoki <newloki91@googlemail.com>
2011-06-25 03:47:34 +0200

    Move parser into laguage library

---
+++ Library/Language/Parser/Word/WordParserInterface.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * 
+ * @author newloki
+ */
+namespace \Language\Parser\Word;
+use \Language\Languages\LanguagesAbstract;
+
+interface WordParserInterface
+{
+    public function parse($_word, LanguagesAbstract $_language);
+}
